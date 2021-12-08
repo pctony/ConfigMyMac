1882e60e0cb8151c37075458e07ba4b5ae70dad0
Tony Stevenson <tony@pc-tony.com>
2021-12-08 15:42:57 -0800

    Add visual-studio-code

--- MyMac/00_variables.sh
+++ MyMac/00_variables.sh
@@ -4,14 +4,14 @@
 
 
 # This is the list of homebrew formulas that will be installed. 
-BREW_FORMULAS="axel exiftool gettext hub mas tmux"
+BREW_FORMULAS="axel dockutil exiftool gettext hub mas pyenv tmux visual-studio-code" 
 
 # This is the list of homebrew formulas that will be installed. 
-BREW_CASKS="alfred google-chrome istat-menus iterm2 visual-studio-code vlc"
+BREW_CASKS=""
 
 # This is the list of AppIDs to install vias the Mac App Store
 
 # GarageBand 682658836, iMovie 408981434, Keynote 409183694, Microsoft Excel 462058435, Microsoft Word 462054704, Numbers 409203825, Pages 409201541, Slack 803453959, 
 # WiFi Explorer 494803304, StatusClock 552792489, Magnet 441258766, xcode 497799835, Agenda 1287445660, Feedly 865500966, Twitterific 1289378661
 
-APPSTORE_IDS="803453959 682658836 408981434 409183694 462058435 462054704 409203825 409201541 494803304 552792489 441258766 497799835 1287445660 865500966 1289378661"
+APPSTORE_IDS=""
